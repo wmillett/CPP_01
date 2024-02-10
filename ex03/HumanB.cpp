31ce3cc8c66f275b89241069ff0d31a07dbd3cd6
William Millette <wmillett@reception.42quebec.com>
2024-02-10 18:18:27 -0500

    removed bad comments from ex03

--- ex03/HumanB.cpp
+++ ex03/HumanB.cpp
@@ -1,87 +1,13 @@
-// #include "HumanB.hpp"
 
-
-//  HumanB::HumanB(){
-//     this->weapon = Weapon();
-//     // this->name = "No name";
-//     // this->setWeapon("No weapon");
-//  };
-//  HumanB::HumanB(std::string name) : name(name){
-//     this->weapon = Weapon();
-//  };
-// HumanB::~HumanB(){
-
-// };
-
-// void HumanB::attack(void){
-//     std::cout << this->name << ATTACK_PHRASE << this->weapon.getType() << std::endl;
-// };
-
-// void HumanB::setWeapon(Weapon newWeapon){
-//     this->weapon.setType(newWeapon.getType());
-// };
-
-// HumanB.cpp
-// #include "HumanB.hpp"
-
-// HumanB::HumanB() : name("No name"), weapon(Weapon()) {
-//     // Initialize other members if needed
-// }
-
-// HumanB::HumanB(std::string name) : name(name), weapon(Weapon()) {
-//     // Initialize other members if needed
-// }
-
-// HumanB::~HumanB() {
-//     // Destructor body
-// }
-
-// void HumanB::attack() {
-//     std::cout << this->name << ATTACK_PHRASE << this->weapon.getType() << std::endl;
-// }
-
-// void HumanB::setWeapon(Weapon newWeapon) {
-//     this->weapon.setType(newWeapon.getType());
-// }
-
-// HumanB.cpp
-// #include "HumanB.hpp"
-
-// HumanB::HumanB() : name("No name"), weapon(Weapon()) {
-//     // Initialize other members if needed
-// }
-
-// HumanB::HumanB(std::string name) : name(name), weapon(Weapon()) {
-//     // Initialize other members if needed
-// }
-
-// HumanB::~HumanB() {
-//     // Destructor body
-// }
-
-// void HumanB::attack() {
-//     std::cout << this->name << ATTACK_PHRASE << this->weapon.getType() << std::endl;
-// }
-
-// void HumanB::setWeapon(const Weapon& newWeapon) {
-//     // You might need to decide how to handle changing the weapon reference
-//     // For now, it just prints the type.
-//     std::cout << "Setting weapon for HumanB " << this->name << " to " << newWeapon.getType() << std::endl;
-// }
-
-// HumanB.cpp
 #include "HumanB.hpp"
 
 HumanB::HumanB() : name("No name"), weapon(nullptr) {
-    // Initialize other members if needed
 }
 
 HumanB::HumanB(std::string name) : name(name), weapon(nullptr) {
-    // Initialize other members if needed
 }
 
 HumanB::~HumanB() {
-    // Destructor body
 }
 
 void HumanB::attack() {
--- ex03/HumanB.hpp
+++ ex03/HumanB.hpp
@@ -1,48 +1,4 @@
-// #ifndef HUMANB_HPP
-// # define HUMANB_HPP
 
-// #include <iostream>
-// #include "Weapon.hpp"
-
-
-
-// class HumanB{
-
-//     private:
-//         std::string name;
-//         Weapon& weapon;
-//     public:
-//         HumanB();
-//         HumanB(std::string name);
-//         ~HumanB();
-//         void attack(void);
-//         void setWeapon(Weapon newWeapon);
-
-// };
-// #endif
-
-// HumanB.hpp
-// #ifndef HUMANB_HPP
-// # define HUMANB_HPP
-
-// #include <iostream>
-// #include "Weapon.hpp"
-
-// class HumanB {
-// private:
-//     std::string name;
-//     const Weapon& weapon;  // Change to const reference
-// public:
-//     HumanB();
-//     HumanB(std::string name);
-//     ~HumanB();
-//     void attack(void);
-//     void setWeapon(const Weapon& newWeapon);  // Change to const reference
-// };
-
-// #endif
-
-// HumanB.hpp
 #ifndef HUMANB_HPP
 # define HUMANB_HPP
 
